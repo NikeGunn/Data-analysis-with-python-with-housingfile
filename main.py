6df3ea937b20cbd0c31f1f3a08ceadceab6cbb3f
Nikhil Bhagat <knewboy.nykhil@gmail.com>
2023-03-27 15:35:45 +0545

    Fixed python version error arrive while generating report.html file

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 import pandas as pd
+from ydata_profiling import ProfileReport
 
+#Reading a report & geting them int dataframe
 df = pd.read_csv("housing.csv")
 
-print(df)
+# print(df)
+
+#Generate a report 
+profile = ProfileReport(df)
+
+profile.to_file("report2.html")
+
 
